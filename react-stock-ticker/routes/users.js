baedc98df1bc21d2f242fe28ffe2d64f58c88fc0
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-31 16:08:57 -0800

    cleaning up code.

--- react-stock-ticker/routes/users.js
+++ react-stock-ticker/routes/users.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    // Comment out this line:
-  //res.send('respond with a resource');
-
-  // And insert something like this instead:
   res.json([{
     id: 1,
     username: "samsepi0l"
