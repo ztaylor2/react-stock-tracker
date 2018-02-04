2a1c7ecf17da4f833ce5de30f115a25fe3051488
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-03 23:42:08 -0800

    added some customization to home page.

--- react-stock-ticker/client/src/App.js
+++ react-stock-ticker/client/src/App.js
@@ -37,9 +37,9 @@ const Home = () => (
   </div>
 )
 
-const About = () => (
+const Portfolio = () => (
   <div>
-    <h2>About</h2>
+    <h2>Portfolio</h2>
   </div>
 )
 
@@ -82,14 +82,14 @@ const BasicExample = () => (
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
         <li><Link to="/topics">Topics</Link></li>
       </ul>
 
       <hr/>
 
       <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
+      <Route path="/portfolio" component={Portfolio}/>
       <Route path="/topics" component={Topics}/>
     </div>
   </Router>
