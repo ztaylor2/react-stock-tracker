c606eea8bd6deb86870d079515f36d04e6115ef7
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-08 17:31:56 -0800

    rendering another view in first view.

--- stock-tracker/src/components/projects.js
+++ stock-tracker/src/components/projects.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+class Projects extends Component {
   render() {
     return (
-      <div className="App">
-        My app
+      <div className="Projects">
+        My projects
       </div>
     );
   }
 }
 
-export default App;
+export default Projects;
