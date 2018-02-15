ac62203061b97a50ce448506c00e0e87a50784b2
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-14 22:37:18 -0800

    created portfolio component.

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import Projects from './components/projects';
 import Trade from './components/trade';
 import Navigation from './components/navigation';
+import Portfolio from './components/portfolio';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            stocks: [
+                {
+                    name: 'FB',
+                    value: 14
+                },
+                {
+                    name: 'AMZN',
+                    value: 1000
+                }
+            ]
+        }
+    }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +29,7 @@ class App extends Component {
         My app
         <Projects />
         <Trade />
+        <Portfolio stocks={this.state.stocks} />
       </div>
     );
   }
