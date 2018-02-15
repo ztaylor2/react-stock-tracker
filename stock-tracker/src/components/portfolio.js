08d08b2132936511ff2caed47e1e1a153060446e
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-14 22:49:00 -0800

    working on rendering stock to front end.d

--- stock-tracker/src/components/portfolio.js
+++ stock-tracker/src/components/portfolio.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import Stock from './stock';
+
 
 class Portfolio extends Component {
+
   render() {
+
+    console.log(this.props);
+
+    let stock;
+    if (this.props.stock) {
+        stock = this.props.stock.map(a_stock => {
+            <Stock />
+        });
+    }
     return (
       <div className="Portfolio">
         My Portfolio
