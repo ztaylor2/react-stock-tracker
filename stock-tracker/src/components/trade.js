88c07bbda54e753bee83d28d974c4ef3c89c1404
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-15 23:02:45 -0800

    cleaned up indentation on all components.

--- stock-tracker/src/components/trade.js
+++ stock-tracker/src/components/trade.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class Trade extends Component {
-  render() {
-    return (
-      <div className="Trade">
-        Component to place trades
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Trade">
+                Component to place trades
+            </div>
+        );
+    }
 }
 
 export default Trade;
