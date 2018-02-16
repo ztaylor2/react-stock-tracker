e2018d2fbbb4c533df997daabef236f42cce672c
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-15 22:27:38 -0800

    fixed bug by import stock into app.js instead of inner component.

--- stock-tracker/src/components/stock.js
+++ stock-tracker/src/components/stock.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 
 class Stock extends Component {
   render() {
-    let stock;
-    if (this.props.stock) {
-        stock = this.props.stock.map(a_stock => {
-            console.log(project);
-        });
-    }
+
     return (
       <li className="Stock">
         My Stock
