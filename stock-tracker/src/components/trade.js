e6d67c14d0e1967280385696a408f239a2834474
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-16 19:46:37 -0800

    created form for buying/selling shares.

--- stock-tracker/src/components/trade.js
+++ stock-tracker/src/components/trade.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class Trade extends Component {
     render() {
         return (
-            <div className="Trade">
-                Component to place trades
+            <div>
+                <h3>Component to place trades</h3>
+                <form>
+                    <div>
+                        <label>Title</label>
+                        <input type="text" ref="title" />
+                    </div>
+                    <div>
+                        <label>Shares</label>
+                        <input type="text" ref="shares" />
+                    </div>
+                </form>
             </div>
         );
     }
