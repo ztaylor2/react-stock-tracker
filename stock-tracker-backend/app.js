4e0fb252ff1dc30e48c1498ef47153780d8cd7fb
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-20 17:28:02 -0800

    added mongodb starter code. not working yet.

--- stock-tracker-backend/app.js
+++ stock-tracker-backend/app.js
@@ -45,4 +45,29 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+// MONGO DB QUICKSTART 
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
+
+// MONGO DB QUICKSTART 
+
+
 module.exports = app;
